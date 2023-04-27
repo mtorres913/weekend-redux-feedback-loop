947396b5806318beae1ba09ccf3bd52c0bfedcfb
MIguel Torres <mtorres913@gmail.com>
2023-04-27 10:25:19 -0500

    added text field to pageOne component, setup index.js with feedback reducers

--- src/components/Pages/PageOne.jsx
+++ src/components/Pages/PageOne.jsx
@@ -2,12 +2,19 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import { Typography } from '@mui/material';
+import { Typography, TextField } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import React from 'react';
+import { green } from '@mui/material/colors'
 import { useDispatch } from 'react-redux';
 
 function PageOne(){
+
+    const handleChange = (event) => {
+        const action = { type: 'SET_PERSON_NAME', payload: event.target.value };
+        dispatch(action);
+    }
+
     return (
         <Grid item xs={12} md={4}>
         <Card sx={{ minWidth: 275 }} >
@@ -17,11 +24,13 @@ function PageOne(){
                 >
               How are you feeling today?
                 </Typography>
+                < br/>
+                <TextField id="outlined-basic" label="Answer" onChange={handleChange} variant="outlined" />
             </CardContent>
             <CardActions>
                 <Button
                     variant="outlined"
-                    color="error"
+                    color= "error"
                     >Next</Button>
             </CardActions>
         </Card>
